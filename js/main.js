90c99b449c2c1bd57dbb8c4665007f6c88eddb11
Osaf <osaf96@gmail.com>
2022-09-06 18:14:19 +0500

    some changes

--- js/main.js
+++ js/main.js
@@ -1,39 +1,7 @@
-// const hour = document.getElementById('clock-hour')
-// const min = document.getElementById('clock-min')
-// const sec = document.getElementById('clocks-second')
 
-//  const clock = () =>{
-//     let date = new Date()
-
-//     let hh = date.getHours() * 30 
-//     let mm = date.getMinutes() * 6 
-//     let ss = date.getSeconds() * 6 
-
-//    hour.style.transform =`rotateZ(${hh+mm/12}deg)`  ;
-//    min.style.transform =`rotateZ(${mm}deg)`;
-//    sec.style.transform =`rotateZ(${ss}deg)`
-//  }  
- 
-//  setInterval(clock , 1000)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const hour = document.getElementById('clock-hour'),
-      minutes = document.getElementById('clock-minutes'),
-      seconds = document.getElementById('clock-second')
+const hour = document.getElementById('clock-hour')
+const  minutes = document.getElementById('clock-minutes')
+ const seconds = document.getElementById('clock-second')
 
 const clock = () =>{
     let date = new Date()
