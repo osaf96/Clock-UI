f99d5fef22f6af0c01570ac05ca93ecfbf0cd3c9
Osaf <osaf96@gmail.com>
2022-09-09 12:12:21 +0500

    date-section added

--- js/main.js
+++ js/main.js
@@ -2,13 +2,18 @@
 const hour = document.getElementById('clock-hour')
 const  minutes = document.getElementById('clock-minutes')
  const seconds = document.getElementById('clock-second')
+// for  DATE card
+ const dateday = document.getElementById('day')
+ const datemonth = document.getElementById('month')
+ const dateyear = document.getElementById('year')
+ 
 
 const clock = () =>{
-    let date = new Date()
+    let time = new Date()
 
-    let hh = date.getHours() * 30
-    let mm = date.getMinutes() * 6
-    let ss = date.getSeconds() * 6
+    let hh = time.getHours() * 30
+    let mm = time.getMinutes() * 6
+    let ss = time.getSeconds() * 6
         
     // We add a rotation to the elements
     hour.style.transform = `rotateZ(${hh + mm / 12}deg)`
@@ -16,3 +21,21 @@ const clock = () =>{
     seconds.style.transform = `rotateZ(${ss}deg)`
 }
 setInterval(clock, 1000) // 1000 = 1s
+
+ 
+// for date 
+
+
+const current_date = ()=>{
+    let date = new Date()
+
+ 
+let day = date.getDay() + 4
+let  month= date.getMonth() + 1
+let year= date.getFullYear()
+
+dateday.innerHTML = day + "/"
+datemonth.innerHTML= month + "/"
+dateyear.innerHTML = year
+} 
+setInterval(current_date, 1000)
